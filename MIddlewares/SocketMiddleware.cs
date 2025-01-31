1772f773c52137e5c075625f32eebfb032ab82d8
Victor-Spichenkoff <victorspich13@gmail.com>
2025-01-31 12:14:38 -0300

    Stable to add turn

--- MIddlewares/SocketMiddleware.cs
+++ MIddlewares/SocketMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using asp_rest_model.Services;
+﻿using System.Text.Json;
+using asp_rest_model.Models;
+using asp_rest_model.Services;
 
 namespace asp_rest_model.MIddlewares;
 
@@ -24,8 +26,25 @@ public class SocketMiddleware(RequestDelegate next, SocketService socketService)
                 return;
             }
 
+            try
+            {
+                await _socketService.HandleWebSocketAsync(webSocket, roomId);
+
+            }
+            catch
+            {
+                context.Response.StatusCode = 400;
+                var res = new SocketInGameResponse()
+                {
+                    errorMessage = "Room with problems, please create a new one",
+                    isError = true
+                };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(res));
+                    await _next(context);
+                // await context.Response.WriteAsync("Room with problems, please create a new one");
+            }
+
             // Delegar para o serviço de sockets
-            await _socketService.HandleWebSocketAsync(webSocket, roomId);
         }
         else
         {
