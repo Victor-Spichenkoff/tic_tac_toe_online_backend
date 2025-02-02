019ff5e72ede0b84d595a98ea1fea3e5de173a5e
Victor-Spichenkoff <victorspich13@gmail.com>
2025-02-01 21:41:08 -0300

    Create test route

---
+++ Controllers/RootController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_rest_model.Controllers;
+
+[ApiController]
+[Route("/")]
+public class RootController: ControllerBase
+{
+    [HttpGet("/teste")]
+    public string TestConnection() => "Funcionando";
+}
